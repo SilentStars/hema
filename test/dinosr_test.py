cc9522683b067a6053b921927873b3ecea76863f
Qinhan Hou <h15822008350@163.com>
2024-06-04 10:52:26 +0200

    update gitignore, v0.0.7, DOING: test module.

--- test/dinosr_test.py
+++ test/dinosr_test.py
@@ -1,13 +1,1 @@
 from hema.models.DinoSR.model import DinoSR, config
-import torch
-
-if __name__ == '__main__':
-    cfg = config()
-    model = DinoSR(cfg)
-    
-    # input shape B * C * T
-    fake_emg = torch.rand((12, 8, 400))
-    res = model.forward(fake_emg)
-    
-    print(res)
-    
